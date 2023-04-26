6fbc6bde333cfb239c008a3826039cc832fe5001
Giadagenovesi <giada.g@outlook.it>
2023-04-26 13:38:07 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import AppFooter from './components/AppFooter.vue';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import {store} from "./store";
+
+
+export default {
+    components: { AppHeader, AppMain, AppFooter },
+    data() {
+      return {
+        store
+      }
+    },
+}
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />  
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss"
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: "AppHeader",
+}
+</script>
+
+<template>
+    <header>
+        <div class="header-top">
+            <div class="container">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quo exercitationem tempore expedita culpa minima tempora et repellendus? Voluptates ipsam doloribus corporis doloremque adipisci magnam, laborum sequi eveniet quis aut!
+            </div>
+        </div>
+        <div class="header-bottom"></div>
+    </header>
+</template>
+
+<style scoped lang="scss">
+.header-top {
+    height: 50px;
+    width: 100%;
+    background-color: #21333E;
+    color: white;
+}
+</style>
